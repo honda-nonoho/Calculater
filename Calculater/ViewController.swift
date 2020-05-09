ae8a91e3741d949cdc661ba07e99e79851bdc014
honda-nonoho <nnhhnd3012@gmail.com>
2020-05-09 14:11:04 +0900

    エラーが解決できひん。わからへん。

--- Calculater/ViewController.swift
+++ Calculater/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Calculater
+//  Calculator
 //
 //  Created by nonoho.honda on 2020/05/07.
 //  Copyright © 2020 nonoho.honda. All rights reserved.
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapCalculate(_ sender: Any) {
+        //計算処理を書いていきましょう
+        
+        let first = Int(self.firstTextField.text ?? "")!
+        let second = Int(self.secondTextField.text ?? "")!
+    
+        var result = 0
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            // 足し算
+            result = first + second
+            break
+        case 1:
+            // 引き算
+            result = first - second
+            break
+        case 2:
+            // 掛け算
+            result = first * second
+            break
+        case 3:
+            // 割り算
+            result = first / second
+            break
+        default:
+            break
+        }
+        print(result)
+        
+        resultLabel.text = String(result)
+    }
 }
 
-
-
-
-
-//new file
